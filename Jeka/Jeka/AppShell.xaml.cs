39d139c9c1913377cdfe9ea9e98f7b88f0a130f8
Alexandr <jwchaganav@gmail.com>
2022-11-24 17:26:03 +0400

    пока шаблон

---
+++ Jeka/Jeka/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Jeka.ViewModels;
+using Jeka.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Jeka
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ Jeka/Jeka/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Jeka.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Jeka.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
